09294bfeb943dc50971bbf53d78796d255b6cfe6
Larry Hengen <lhengen@tpersistent.com>
2020-08-08 19:11:48 +0000

    -Fixed issue with ThcWideString fields being truncated to 20 characters based on default MaxSize.  Removed MaxSize property. Validator now handles string length constraints as with other types.

--- SQL/FireBird/exec spCreateDeployment.sql
+++ SQL/FireBird/exec spCreateDeployment.sql
@@ -1,2 +1,2 @@
-execute procedure SPCREATEDEPLOYMENT (CHAR_TO_UUID ('8BF6627D-CB05-4601-996E-072DC6785F81'), '1.0.0.3', 'Nothing is new',
+execute procedure SPCREATEDEPLOYMENT (CHAR_TO_UUID ('8BF6627D-CB05-4601-996E-072DC6785F81'), '1.0.0.12', 'Support for Larger Chainage and Spacing Values',
     FALSE, FALSE, TRUE);
